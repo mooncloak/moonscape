1e55ea48ef21e24b951d3b918a1b2a5a45ca4737
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2024-12-02 15:11:33 -0500

    Merged PlatformWebSettings into WebSettings to make everything platform agnostic, since each platform could support different implementations

---
+++ webview-core/src/commonMain/kotlin/com/mooncloak/kodetools/webview/LayerType.kt
@@ -0,0 +1,8 @@
+package com.mooncloak.kodetools.webview
+
+public object LayerType {
+
+    public const val NONE: Int = 0
+    public const val SOFTWARE: Int = 1
+    public const val HARDWARE: Int = 2
+}
