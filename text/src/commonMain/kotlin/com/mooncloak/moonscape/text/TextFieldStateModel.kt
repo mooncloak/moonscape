372f4748e9c4284c25264c7ce6f8a41ee21df4ee
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-04-21 11:08:18 -0400

    Updated TextFieldStateModel

--- text/src/commonMain/kotlin/com/mooncloak/moonscape/text/TextFieldStateModel.kt
+++ text/src/commonMain/kotlin/com/mooncloak/moonscape/text/TextFieldStateModel.kt
@@ -1,32 +1,59 @@
 package com.mooncloak.moonscape.text
 
+import androidx.compose.material3.TextField
 import androidx.compose.ui.text.input.TextFieldValue
 
 /**
- * Allows associating extra data with a [TextFieldValue], such as an [error] message.
+ * Allows associating extra data with a [TextFieldValue], such as a [supporting] message.
+ *
+ * Input [TextField]s utilize the [TextFieldValue] class to represent the current input, but more often than not, there
+ * is additional state associated with a [TextFieldValue], such as error messages. Instead of having two separate
+ * values in a state model represent the values for a single [TextField], combining the two (value + supporting) is
+ * very beneficial.
  *
  * @property [value] The [TextFieldValue] associated with the UI component.
  *
- * @property [error] The error message associated with the UI component. This allows error messages for individual Text
- * Input UI components.
+ * @property [supporting] The supporting text message associated with the UI component. This allows supporting text
+ * messages for individual Text Input UI components.
+ *
+ * @property [isError] Whether the [supporting] text represents an error message.
  */
 public data class TextFieldStateModel public constructor(
     public val value: TextFieldValue = TextFieldValue(),
-    public val error: String? = null
+    public val supporting: String? = null,
+    public val isError: Boolean = true
 ) {
+
+    public constructor(
+        text: String,
+        supporting: String? = null,
+        isError: Boolean
+    ) : this(
+        value = TextFieldValue(text = text),
+        supporting = supporting,
+        isError = isError
+    )
 
     public constructor(
         text: String,
         error: String? = null
     ) : this(
         value = TextFieldValue(text = text),
-        error = error
+        supporting = error,
+        isError = true
     )
 }
 
 /**
- * Determines if the associated [TextFieldStateModel.error] value is `null`. Returns `true` if the
- * [TextFieldStateModel.error] value is `null`, `false` otherwise.
+ * Retrieves the [TextFieldStateModel.supporting] if the [TextFieldStateModel.isError] is `true`, otherwise `null`.
+ */
+public val TextFieldStateModel.error: String?
+    inline get() = if (this.isError) supporting else null
+
+/**
+ * Determines if the associated [TextFieldStateModel.supporting] value is `null` or blank when in an error state.
+ * Returns `true` if the [TextFieldStateModel.supporting] value is `null` or blank when [TextFieldStateModel.isError]
+ * is `true`, `false` otherwise.
  */
 public val TextFieldStateModel.isValid: Boolean
-    inline get() = this.error == null
+    inline get() = !this.isError || this.supporting.isNullOrBlank()
